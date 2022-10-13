0aab5183ea6079820f9eda1aee111044d5bee44c
Your Name <you@example.com>
2022-10-13 15:49:00 -0700

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - Prints the size of many variables
+ * Return: Always 0 (success)
  */
 int main(void)
 {
+char;
+int;
+long int;
+long long int;
+float;
+
 printf("size of a char: %i byte(s)\n", sizeof(char));
 printf("size of an int: %i byte(s)\n", sizeof(int));
 printf("size of a long int: %i byte(s)\n", sizeof(long int));
