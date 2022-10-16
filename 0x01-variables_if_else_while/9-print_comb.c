046d5dffbb64b18ea122a6a5913877a8057a576b
Your Name <you@example.com>
2022-10-16 08:57:53 -0700

    print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,13 @@
 int main(void)
 {
 int n;
-for(n = 48; n < 58; n++)
-
-	     putchar(n) 
-	     if(n != 57)
-             {
- 	         putchar(',')
-	         putchar(' ')
-	     }
+for (n = 48; n < 58; n++)
+putchar(n)
+if (n != 57)
+{
+putchar(',')
+putchar(' ')
+}
 putchar('\n')
-return(0);	
+return (0);
 }
