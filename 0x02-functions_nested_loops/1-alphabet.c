a6f16ac0b7f0f0fb9470bb5037fef200759b5bd8
Your Name <you@example.com>
2022-10-18 13:58:31 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * Return: always 0
+ */
+int main(void)
+{
+char la;
+for (la = 'a'; la <= z; la++)
+_putchar(la);
+_putchar('\n');
+return (0);
+}
