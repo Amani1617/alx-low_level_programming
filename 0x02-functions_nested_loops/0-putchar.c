d20d0b07ad98216fea7127cf3a76b082a9b65727
Your Name <you@example.com>
2022-10-18 08:36:07 -0700

    putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - prints putchar
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return (0);
 }
