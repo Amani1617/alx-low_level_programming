fb06d5954ab4bb72b6fb2b32b472c4d86944927a
Your Name <you@example.com>
2022-10-19 02:32:18 -0700

    print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+int ten;
+char la;
+for (ten = 0; num <= 9; ten++)
+{
+for (la = 'a'; la <= 'z'; la++)
+	putchar(la);
+putchar('\n');
+}
+}
