f7e3d9bbfb82ddc38c1980cfe9f7555583598061
Your Name <you@example.com>
2022-10-19 15:37:44 -0700

    sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include"main"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the numgerof the signto be printed
+ * Return: 1 if the number is greater then zero
+ * 0 if the number is zero
+ * -1 if the numer is less than zero  
+ */
+int print_sign(int n)
+{
+	if (n>0)
+	{
+     putchar()
+	     return();
+     else if ()
+     {
+
+	     putchar ()
+	return()
+		     else
+		     {
+			     putchar ()
+				     return()
+}
+}
+}
+}
+
+
