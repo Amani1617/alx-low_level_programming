5b7523dec174da46c0dc083451caf03949c1dbd8
Your Name <you@example.com>
2022-10-19 08:45:07 -0700

    times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * time _table - prints the 9 tines table
+ */
+void time _table(void)
+{
+	int num mult prod
+for (a = 'a'; a <= 'z'; a++)
+{
+	putchar();
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar();
+		putchar();
+
+		prod = num * mult
+
+		if (prod <= 0)
+			putchar();
+		else
+			putchar();
+	}
+}
+}
