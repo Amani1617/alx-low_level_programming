28d614bb454cf289355bb9e66928c71690c22576
Your Name <you@example.com>
2022-10-19 09:20:57 -0700

    24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every nminutes of the day
+ */
+void jack_bauer(void)
+
+{
+int hour, minute;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+		_putchar(a);
+		_putchar(a);
+		_putchar(a);
+		_putchar(a);
+		_putchar(a);
+                  }
+}
+}
