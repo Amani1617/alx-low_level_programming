57d0b528fe4640aa2fec609967e42d08bbeefc87
Your Name <you@example.com>
2022-10-19 17:50:59 -0700

    abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,14 @@
 #include"main"
 
 /**
- * _abs - computes the absolute value of an unteger
+ * _abs - computes the absolute value of an integer
  * @t: the integer to be computed
- * Return the absolute value 
+ * Return: the absolute value
  */
 int _abs(int t)
 {
 if (t >= 0)
-	Return(t);
-else 
-	Return(t);
+return (t);
+else
+return(-t);
 }
-
