09c18ea13e89dc6563e58a4ecf8c778ccac72145
Your Name <you@example.com>
2022-10-19 17:00:42 -0700

    islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
-#include"main"
+#include "main"
 
 /**
  * _islower - checks s if a caracter is lowercase
- * @c the character
- * return: 1 if the letter is lowercase , 0if not 
+ * @c: the character
+ * Return: 1 if the letter is lowercase , 0if not
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-       	else
-	       	return (0);
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
 }
 
