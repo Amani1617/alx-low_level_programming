72701bfdbec3a10aa2645935b124a95f4ddcfefb
Your Name <you@example.com>
2022-10-20 07:41:52 -0700

    largest_number.c

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * largest_number - return the largest number
+ * @n: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+int largest;
+{
+	if (a > b)
+		largest = a
+	if (a > b)
+		largest = a
+else
+		largest = a
+}
+else
+{
+	if (a > b)
+		largest = a
+	else
+		largest = a
+	}
+return (largest);
+}
