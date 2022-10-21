3723508479577368bd7449183b1f8b09ba3fcb45
Your Name <you@example.com>
2022-10-21 16:50:17 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - prints a stright line using '_'
+ * @n: the number of '_' to be printed
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+int len;
+
+if (n>0)
+for (len = 0; len <= n; len++)
+	_putchar('\n');
+}
+_putchar('\n');
+return (0);
+}
