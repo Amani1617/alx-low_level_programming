b70ef2550a0761f0a69cf5f3a71632f361704a8e
Your Name <you@example.com>
2022-10-21 16:41:33 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - prints 0 to 14 ten times
+ * Return: Always 0
+ */
+void more_numbers(void);
+{
+	int num a;
+	for (a = 0, a <= 9; a++)
+	{
+		for (num = 0, num <= 9; a++)
+		{
+			if num > 9
+			{
+				_putchar((num / 10);
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
