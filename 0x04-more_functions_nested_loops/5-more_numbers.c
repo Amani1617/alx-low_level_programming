9d8b294356b835cc1119521594a3ae006adf128c
Your Name <you@example.com>
2022-10-21 18:23:25 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,20 @@
  */
 void more_numbers(void);
 {
-	int num a;
-	for (a = 0, a <= 9; a++)
-	{
-		for (num = 0, num <= 9; a++)
-		{
-			if num > 9
-			{
-				_putchar((num / 10);
-			}
-			_putchar('\n');
-		}
-		_putchar('\n');
-	}
+int num, a
+for (a = 0, a <= 9; a++)
+{
+for (num = 0, num <= 14; num++)
+{
+if (num > 9)
+{
+_putchar((num / 10) + '0');
+}
+_putchar((num % 10) + '0');
+}
+
+_putchar('\n');
+}
+
+return (0);
 }
