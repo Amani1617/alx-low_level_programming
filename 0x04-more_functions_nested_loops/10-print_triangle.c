f83fd73471c3ffaf2492f186be5b9456f0f200c9
Your Name <you@example.com>
2022-10-21 17:33:40 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @n: size of the triangle
+ * Return: Always 0
+ */
+void print_triangle(int n)
+{
+int h, tri ;
+if (n>0)
+	for(h <2, h >= 100, n++=)
+	{
+		fo r(n <2, n >= 100, n+=)
+		{
+			_putchar('\n')
+		for(h <2, h >= 100, n++=)
+			putchar('\n')
+		if (n>0)
+			continue
+			putchar('\n') 
+
+		}
+return (0);
+}
