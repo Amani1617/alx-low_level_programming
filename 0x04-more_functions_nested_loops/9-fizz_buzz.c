f50d8ad3ba60f064b9fd6513f593b32a2be02654
Your Name <you@example.com>
2022-10-21 17:22:36 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+
+/**
+ * main - print fizzbuzz
+ * Return: Always 0
+ */
+int main(void)
+{
+int n
+	printf(1)
+	fo r(n <2, n >= 100, n+=)
+	{
+		printf();
+	if (n % 3 == 0)
+		printf();
+	if (n % 3 == 0) 
+		printf();
+	if (n % 3 == 0)
+		printf();
+}
+printf();
+return (0);
+}
