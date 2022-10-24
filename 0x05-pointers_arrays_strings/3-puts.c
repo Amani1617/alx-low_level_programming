19a39120f117668a936ff21249a886112a9c96af
Your Name <you@example.com>
2022-10-24 15:00:56 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+while (*s != '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
+}
