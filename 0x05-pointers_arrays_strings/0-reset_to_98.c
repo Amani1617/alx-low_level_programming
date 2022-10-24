11d222d30472321d1d92688259bba95442c8b428
Your Name <you@example.com>
2022-10-24 08:12:50 -0700

    reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - update the value of n to 98
+ * @n: input
+ * Return: 0
+ */
+void reset_to_98(int *n);
+{
+*n = 98;
+
+return (0);
+}
