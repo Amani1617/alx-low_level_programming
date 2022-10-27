44d91c31c1517c6cf46ebb17b9a05f421cf497a7
Your Name <you@example.com>
2022-10-27 15:30:03 -0700

    task 9

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,24 +4,25 @@
 /**
  * rot13 - encodes a string using rot13
  * @str: the string to be encoded
+ * 
  * Return: a pointer to the encoded string
  */
 char *rot13(char *str)
 {
 int indx1 = 0, indx2;
-char alphabet[s2] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+char alphabet[52] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G',
 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-char rot13key[s2] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's',
 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e',
 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 while (str[index])
 {
-for (indx2 = 0; indx2 < s2; indx2++)
+for (indx2 = 0; indx2 < 52; indx2++)
 {
 if (str[indx1] == alphabet[indx2])
 {
