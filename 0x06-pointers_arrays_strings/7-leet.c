0fdc0081f064c39829a5f4fac196c2a8db38d690
Your Name <you@example.com>
2022-10-27 08:40:35 -0700

    task 8

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,13 +4,13 @@
 /**
  * leet - encodes a string into 1337
  * @n: input
- * Return: the value of the input
+ * Return: the value of n
  */
 char *leet(char *n)
 {
 int i, j;
-char s1[] == "aAeEoOtTlL";
-char s2[] == "4433007711";
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
 
 for (i = 0; n[i] != '\0'; i++)
 {
@@ -18,7 +18,7 @@ for (j = 0; j < 10; j++)
 {
 if (n[i] == s1[j])
 {
-n[i] == s2[j];
+n[i] = s2[j];
 }
 }
 }
