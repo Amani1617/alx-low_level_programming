168cf6ec590bdcec02ad60c84a13d7592b3689f5
Your Name <you@example.com>
2022-10-28 16:33:33 -0700

    task 7

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_line - prints a stright line using '_'
  * @n: the number of '_' to be printed
- * Return: Always 0
  */
 void print_line(int n)
 {
