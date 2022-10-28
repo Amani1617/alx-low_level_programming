aa51e01266d446cc4285a0956b48f78ad2ddd886
Your Name <you@example.com>
2022-10-28 15:57:09 -0700

    task 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_numbers - print from 0 to 9
- * without 2 and 5
  * Return: Always 0
  */
 void print_numbers(void)
@@ -11,10 +9,7 @@ void print_numbers(void)
 int a = 0;
 
 while (a < 10)
-if (a != 2 && a != 4)
-{
-_putchar(a + '0');
-a++;
-}
+_putchar(a++ + '0');
+
 _putchar('\n');
 }
