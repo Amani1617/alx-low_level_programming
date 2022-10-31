7af9e06a0b923921c159c7796b1fe0fc4ee92eb2
Your Name <you@example.com>
2022-10-31 15:39:02 -0700

    task 01

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - copies memory area
+ * @dest: the place we copy to
+ * @src: the place we copy from
+ * @n: the number of times we copy
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *ptr = dest;
+while (n-- > 0)
+{
+*dest = *src;
+dest++;
+src++;
+}
+return (dest);
+}
