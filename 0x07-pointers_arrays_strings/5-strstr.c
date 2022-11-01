41a4697198d4e8ced9e8822b588fbd47426fbe94
Your Name <you@example.com>
2022-11-01 16:42:22 -0700

    task 6

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,9 +5,9 @@
  * _strstr - locates a substring.
  * @haystack: the string to be searched
  * @needle: the substring to be located
- * Return: if a substring is located - a pointer to the beginning
- *                                     of the located substring
- *         if a substring is not located - NULL.
+ * Return: if the  substring is located - a pointer to the beginning
+ *                                        of the located substring
+ *         if the substring is not located - NULL.
  */
 char *_strstr(char *haystack, char *needle)
 {
