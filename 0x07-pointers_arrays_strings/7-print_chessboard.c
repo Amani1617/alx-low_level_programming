31b5dc27338c107b4ef9137f898f3a30ba366fa7
Your Name <you@example.com>
2022-11-01 16:59:06 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: the chess board to be printed
+ */
+void print_chessboard(char (*a)[8])
+{
+int indx1, indx2;
+for (indx1 = 0; a[indx][7]; indx1++)
+{
+for (indx2 = 0; indx2 < 8; indx2++)
+_putchar(a[indx1][indx2]);
+_putchar('\n');
+}
+}
