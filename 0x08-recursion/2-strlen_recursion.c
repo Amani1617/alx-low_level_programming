57505bb63818de20f957a17d2a96513e1dc21f8f
Your Name <you@example.com>
2022-11-02 13:41:53 -0700

    strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 
 /**
  * _strlen_recursion - returns the length of a string
- * @s: the string whose length is to be determined
+ * @s: the string to be accounted
  *
  * Return: the length of the string
  */
@@ -15,5 +15,4 @@ if (*s)
 {
 len++;
 len += _strlen_recursion(s + 1);
-}
 }
