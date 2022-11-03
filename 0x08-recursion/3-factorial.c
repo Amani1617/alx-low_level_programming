adc8c472b13a85bf15c805f84d5cacb67472aee2
Your Name <you@example.com>
2022-11-03 15:02:01 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,14 +4,16 @@
 /**
  * factorial - returns the factorial of a given number.
  * @n: the number whose factorial is determined
- *
- * Return: if n > 0 - the factorial of n
- *         if n < 0 - 1 to show it doesn't exist
+ * Return: factorial
  */
 int factorial(int n)
 {
 
-if (n < 0)
-return (-1);
-else if (n >= 0 && n <= 1)
+if (n == 0)
 return (1);
+else if (n < 0)
+return (-1);
+else
+return (n * factorial(n - 1));
+
+}
