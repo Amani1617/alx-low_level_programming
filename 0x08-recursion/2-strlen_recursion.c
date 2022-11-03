90d4da1eced78d7cc50ea085ffc812cd050336fc
Your Name <you@example.com>
2022-11-03 16:17:00 -0700

    strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,17 +4,14 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: the string to be accounted
- *
- * Return: the length of the string
+ * Return: _strlen_recursion
  */
 
 int _strlen_recursion(char *s)
 {
-int len = 0;
 
-if (*s)
-{
-len++;
-len += _strlen_recursion(s + 1);
-}
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
 }
