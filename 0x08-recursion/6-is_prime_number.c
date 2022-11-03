32cec3f22521a2b4851cfd819f91c63887049566
Your Name <you@example.com>
2022-11-03 16:07:38 -0700

    if prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number -  returns 1 if the input integer is a prime number else 0
+ * @n: number
+ * Return: return - 1 if prime - return -1 if not
+ */
+int is_prime_number(int n)
+{
+int iter;
+iter = 2;
+if (n < 2)
+return (0);
+else if (n == 2 || n % 2 != iter)
+return (1);
+}
