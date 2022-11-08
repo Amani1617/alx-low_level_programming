dabb61772ad05cc80dcf28ccfde92b6f62c40eea
Your Name <you@example.com>
2022-11-08 09:21:05 -0800

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,15 +1,70 @@
 #include "main.h"
+
+/**
+ * _strlen - returns the length of string
+ * @s: array of elements
+ * Return: 1 (on sucess)
+ */
+int _strlen(
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * _strdup - returns a pointer to a newly allocated space
+ * _strdup - returns a pointer to a newly allocated space in memory
  * @str: array of elements
- * Return: 1 (on Sucess)
+ * Return: pointer (on Sucess)
  */
-char *_strdup(char *str)
+char *_strd(char *s)
 {
+char *dest;
 unsigned int i;
+if (str == 0)
+{
+return (NULL);
+}
+size = _strlen(str) + 1;
+dest = (char *) malloc(size *sizeof(char));
+if (dest == 0)
+{
+return (NULL);
+}
+_strcpy(dst, str);
+return (dst);
+}
+
+
 i = 0;
 while (str[i] != '\0')
 {
