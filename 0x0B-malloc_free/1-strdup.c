e2ec458823dee890ccbd3ddc7714193bf12700cc
Your Name <you@example.com>
2022-11-08 08:26:59 -0800

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup -  returns a pointer to a newly allocated space
+ * @str: array of elements
+ * Return: 1 (on Sucess)
+ */
+char *_strdup(char *str)
+{
+unsigned int i;
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (1);
+}
+
