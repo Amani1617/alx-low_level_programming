5b5091394136441dc620136f2b4d3e939fddbba5
Your Name <you@example.com>
2022-11-08 14:25:18 -0800

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,70 +1,15 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * _strlen - returns the length of string
  * @s: array of elements
  * Return: 1 (on sucess)
  */
-int _strlen(
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * _strdup - returns a pointer to a newly allocated space in memory
- * @str: array of elements
- * Return: pointer (on Sucess)
- */
-char *_strd(char *s)
+int _strlen(char *s)
 {
-char *dest;
 unsigned int i;
-if (str == 0)
-{
-return (NULL);
-}
-size = _strlen(str) + 1;
-dest = (char *) malloc(size *sizeof(char));
-if (dest == 0)
-{
-return (NULL);
-}
-_strcpy(dst, str);
-return (dst);
-}
-
-
 i = 0;
 while (str[i] != '\0')
 {
@@ -73,3 +18,44 @@ i++;
 return (i);
 }
 
+/**
+ * _strcpy - copies the string
+ * @src: array of elements
+ * @dest: dest array
+ * Return: dest (on sucess)
+ */
+char _strcpy(char *dest, char *src)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: array of elements
+ * Return: pointer (on Sucess)
+ */
+char *_strdup(char *str)
+{
+char *dst;
+unsigned int size;
+if (str == 0)
+{
+return (NULL);
+}
+size = _strlen(str) + 1;
+dst = (char *) malloc(size *sizeof(char));
+if (dst == 0)
+{
+return (NULL);
+}
+_strcpy(dst, str);
+return (dst);
+}
