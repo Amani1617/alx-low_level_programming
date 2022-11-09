dacdedd20fcb8a576829eb1eb10174a228ec8822
Your Name <you@example.com>
2022-11-09 10:40:45 -0800

    change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * @argc: counts arguments
+ * @argv: arguments
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+int pos, tot, cha, aux;
+int tokens[] = {25, 10, 5, 2, 1};
+pos = tot = cha = aux = 0;
+if (argc != 2)
+{
+printf("ERROR\n");
+return (1);
+}
+tot = atoi(argv[1]);
+if (tot <= 0)
+{
+printf("0\n");
+return (0);
+}
+
+while (tokens[pos] != '\0')
+{
+if (tot >= tokens[pos])
+{
+aux = (tot / tokens[pos]);
+cha += aux;
+tot -= tokens[pos] * aux;
+}
+pos++;
+}
+printf("%d\n", cha);
+return (0);
+}
