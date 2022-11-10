c7b94a0493b46da307275cb9a8fab03bcb76bbee
Your Name <you@example.com>
2022-11-10 15:53:23 -0800

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 /**
- * _realloc - reallocates a memory block using malloc and free
+ * _realloc - reallocates a memory block.
  * @ptr: pointer
  * @old_size: old size
  * @new_size: new size
@@ -12,7 +12,7 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *clone, *relloc;
+char *mutate, *relloc;
 unsigned int t;
 if (ptr != NULL)
 mutate = ptr;
@@ -23,12 +23,12 @@ return (ptr);
 if (new_size == 0 && ptr != NULL)
 { free(ptr);
 return (0); }
-relloc = malloc(new_size)
+relloc = malloc(new_size);
 if (relloc == NULL)
 return (0);
 for (t = 0; t < (old_size || t < new_size); t++)
 {
-*(relloc + t) = clone[t];
+*(relloc + t) = mutate[t];
 }
 free(ptr);
 return (relloc);
