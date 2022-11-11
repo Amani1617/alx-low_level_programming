2f0233848073bc4c35c43bfd65e23bbb0d823eb3
Your Name <you@example.com>
2022-11-11 15:45:18 -0800

    add

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,9 +14,9 @@ int num1, num2;
 if (argc != 3)
 { printf("Error\n");
 exit(98); }
-for (num1 = 1; num1 < argc; a++)
+for (num1 = 1; num1 < argc; num1++)
 {
-for (num2 = 0; argv[num1][num2] != '\0'; b++)
+for (num2 = 0; argv[num1][num2] != '\0'; num2++)
 {
 if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
 { printf("Error\n");
