90a4fee7a733c48f9a43d7314d8134cf1ea46236
Your Name <you@example.com>
2022-11-10 16:54:56 -0800

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,14 +12,14 @@ int main(int argc, char **argv)
 unsigned long pro;
 int a, b;
 if (argc != 3)
-{ printf("ERROR\n");
+{ printf("Error\n");
 exit(98); }
 for (a = 1; a < argc; a++)
 {
 for (b = 0; argv[a][b] != '\0'; b++)
 {
 if (argv[a][b] > 57 || argv[a][b] < 48)
-{ printf("ERROR\n");
+{ printf("Error\n");
 exit(98); }
 }
 }
