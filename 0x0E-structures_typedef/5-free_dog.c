08f75c23025153bc8e92d7ba9fc5e2bffbe7092e
Your Name <you@example.com>
2022-11-15 14:55:04 -0800

    free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * free_dog - frees dog
+ * @d: Freed dog
+ */
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+return;
+free(d->owner);
+free(d->name);
+free(d);
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Alx assignment on Typedef function
