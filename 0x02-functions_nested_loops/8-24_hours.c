32567009ce027549bf07e65da9a41e640a6899c3
Your Name <you@example.com>
2022-12-08 15:39:54 -0800

    _put

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,43 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * jack_bauer - prints every nminutes of the day
+ * description: kept doing the hard thing
+ * _putchar: prints values.
  */
 void jack_bauer(void)
 
 {
-int hour, minute;
+int y = 0;
+int a = 0, b = 0, c = 0, d = 0;
 
-for (a = 'a'; a <= 'z'; a++)
+while (y < 1440)
 {
-	for (a = 'a'; a <= 'z'; a++)
-	{
-		_putchar(a);
-		_putchar(a);
-		_putchar(a);
-		_putchar(a);
-		_putchar(a);
-		_putchar(a);
-                  }
+_putchar(a + '0');
+_putchar(b + '0');
+_putchar(':');
+_putchar(c + '0');
+_putchar(d + '0');
+_putchar('\n');
+
+d++;
+if (d > 9)
+{
+d = 0;
+c++;
+}
+if (c > 5)
+{
+c = 0;
+b++;
+}
+if (b > 9)
+{
+b = 0;
+a++;
+}
+y++;
 }
 }
+
