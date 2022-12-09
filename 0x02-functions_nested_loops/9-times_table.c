d898f61f0536090e66d0d95e7ceed3ce1cf399ce
Your Name <you@example.com>
2022-12-08 16:02:27 -0800

    timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,26 +1,37 @@
 #include "main.h"
 
 /**
- * time _table - prints the 9 tines table
+ * times_table - prints the 9 times of no_ in table
  */
-void time _table(void)
+void times_table(void)
 {
-	int num mult prod
-for (a = 'a'; a <= 'z'; a++)
+int n, m, p, d, s;
+
+for (n = 0; n < 10; n++)
 {
-	putchar();
+for (m = 0; m < 10; m++)
+{
+p = n * m;
+if (p < 10 && m != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+if (p < 10)
+_putchar('0' + p);
 
-	for (a = 'a'; a <= 'z'; a++)
-	{
-		putchar();
-		putchar();
+if (p > 9)
+{
+d = p / 10;
+s = p % 10;
 
-		prod = num * mult
-
-		if (prod <= 0)
-			putchar();
-		else
-			putchar();
-	}
+_putchar(44);
+_putchar(32);
+_putchar('0' + d);
+_putchar('0' + s);
+}
+}
+_putchar('\n');
 }
 }
