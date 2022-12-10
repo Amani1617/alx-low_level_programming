1dcbe905e3539a0a4b0717fd8f0ccdac42b4a375
Your Name <you@example.com>
2022-12-09 17:15:22 -0800

    create_F

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _strlen - length of a string
+  * @s: input char
+  * Return: length of a string
+**/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
 #include "main.h"
 
 /**
@@ -9,20 +29,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int a, b, len = 0;
-if (filename == NULL)
-return (-1);
+ssize_t nletters;
+	int file;
 
-if (text_content != NULL)
-{
-for (len = 0; text_content[len];)
-len++;
-}
-a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0000);
-b = write(a, text_content, len);
-
-if (a == -1 || b == -1)
-return (-1);
-close(a);
-return (-1);
+	if (!filename)
+		return (1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		nletters = write(file, text_content, _strlen(text_content));
+		if (nletters == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
 }
