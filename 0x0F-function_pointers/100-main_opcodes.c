9f58a1285bb984b6ec9f7c33cba3a95e35dddf7f
Your Name <you@example.com>
2022-12-11 14:17:06 -0800

    100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,51 +1,40 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_opcodes - print the opcode
- * @a: address of the main func_
- * @n: number of bytes to be printed
+ * main - Prints the opcodes of this function
  *
- * Return: void
-*/
+ * @argc: The number of command-line arguments
+ *
+ * @argv: The command-line arguments
+ *
+ * Return: 0 if successful
+ */
 
-void print_opcodes(char *a, int n)
+int main(int argc, char *argv[])
 {
-	int a;
+	int n_bytes, i;
+	int (*prog)(int, char **) = &main;
 
-	for (a = 0; a < n; a++)
+	if (argc == 2)
 	{
-		printf("%.2hhx", a[a]);
-		if (a < n - 1)
-			printf(" ");
+		n_bytes = atoi(argv[1]);
+		if (n_bytes < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+		for (i = 0; i < n_bytes; i++)
+			printf("%02x%c",
+					(unsigned char)*((char *)prog + i), i < n_bytes - 1 ? ' ' : '\n');
 	}
-	printf("\n");
-
-}
-
-/**
- * main - prints the opcodes of its own main function
- * @argc: number of arguments passed to the function
- * @argv: array of pointers to arguments
- *
- * Return: always O
-*/
-
-int main(int argc, char **argv)
-{
-	int n;
-
-	if (argc != 2)
+	else
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	n = atoi(argv[1]);
-	if (n < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	print_opcodes((char *)&main, n);
+
 	return (0);
+
 }
+
