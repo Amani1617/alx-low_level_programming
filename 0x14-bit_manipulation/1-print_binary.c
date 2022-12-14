1eec4f695ca77a25b9e6d8319cc1ab6e90fd3f23
Your Name <you@example.com>
2022-12-14 02:27:48 -0800

    printbin

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_binary - decimal to binary without use / %
+ * @n: the decimal
+ * Description: convert decimal to binary
+ * section header: the header of this function is holberton.h
+ * Return: no return
+ */
+void print_binary(unsigned long int n)
+{
+	int fl = 0;
+	unsigned long int mask = 1;
+
+	mask <<= 63;
+	if (n == 0)
+		_putchar('0');
+
+	while (mask > 0)
+	{
+		if ((n & mask) == 0 && f == 1)
+			_putchar('0');
+		if ((n & mask) != 0)
+		{
+			_putchar('1');
+			f = 1;
+		}
+
+		mask = mask >> 1;
+	}
+}
